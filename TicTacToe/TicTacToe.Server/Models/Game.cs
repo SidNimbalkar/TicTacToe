dbfa23d7209ff07f7e1ff7c9da56888a12163e24
Siddhant Nimbalkar <nimbalkar.s@husky.neu.edu>
2018-07-21 05:51:10 -0400

    Tic Tac Toe

---
+++ TicTacToe/TicTacToe.Server/Models/Game.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Server.Models
+{
+    public class Game
+    {
+        private bool isFirstPlayersTurn;
+        
+        /// <param name="player1">The first player to join the game.</param>
+        /// <param name="player2">The second player to join the game.</param>
+        public Game(Player player1, Player player2)
+        {
+            this.Player1 = player1;
+            this.Player2 = player2;
+            this.Id = Guid.NewGuid().ToString("d");
+            this.Board = new Board();
+
+            this.isFirstPlayersTurn = true;
+
+            
+            this.Player1.GameId = this.Id;
+            this.Player2.GameId = this.Id;
+
+           
+            this.Player1.Piece = "X";
+            this.Player2.Piece = "O";
+        }
+
+        
+        public string Id { get; set; }
+
+        public Player Player1 { get; set; }
+
+       
+        public Player Player2 { get; set; }
+
+       
+        public Board Board { get; set; }
+
+       
+        public Player WhoseTurn
+        {
+            get
+            {
+                return (this.isFirstPlayersTurn) ?
+                    this.Player1 :
+                    this.Player2;
+            }
+        }
+
+        
+        public bool IsOver
+        {
+            get
+            {
+                return this.IsTie || this.Board.IsThreeInRow;
+            }
+        }
+
+      
+        public bool IsTie
+        {
+            get
+            {
+                return !this.Board.AreSpacesLeft;
+            }
+        }
+
+        
+        /// <param name="row">The row where the piece will be placed.</param>
+        /// <param name="col">The column where the piece will be placed.</param>
+        public void PlacePiece(int row, int col)
+        {
+            string pieceToPlace = this.isFirstPlayersTurn ?
+                this.Player1.Piece :
+                this.Player2.Piece;
+            this.Board.PlacePiece(row, col, pieceToPlace);
+            
+            this.isFirstPlayersTurn = !this.isFirstPlayersTurn;
+        }
+
+       
+        /// <param name="row">The row position of the move.</param>
+        /// <param name="col">The column position of the move.</param>
+        /// <returns>true if the move is valid; otherwise false.</returns>
+        public bool IsValidMove(int row, int col)
+        {
+            return 
+                row < this.Board.Pieces.GetLength(0) &&
+                col < this.Board.Pieces.GetLength(1) &&
+                string.IsNullOrWhiteSpace(this.Board.Pieces[row, col]);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("(Id={0}, Player1={1}, Player2={2}, Board={3})",
+                this.Id, this.Player1, this.Player2, this.Board);
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.Server/Models/Player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Server.Models
+{
+    
+    public class Player
+    {
+        public Player(string name, string id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+       
+        public string Name { get; private set; }
+
+        
+        public string Id { get; private set; }
+
+       
+        public string GameId { get; set; }
+
+       
+        public string Piece { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("(Id={0}, Name={1}, GameId={2}, Piece={3})", 
+                this.Id, this.Name, this.GameId, this.Piece);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Player other = obj as Player;
+
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Id.Equals(other.Id) && this.Name.Equals(other.Name);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode() * this.Name.GetHashCode();
+        }
+    }
+}
